1fb5f060ef660640f155dd8778472d3c1f2a4a0a
Sergey Kudashev <kudashevs@gmail.com>
2025-01-10 11:02:05 +0300

    Update LastModifiedTest update the logic of the createRequest helper method

--- tests/Unit/Middleware/LastModifiedTest.php
+++ tests/Unit/Middleware/LastModifiedTest.php
@@ -15,9 +15,10 @@ class LastModifiedTest extends TestCase
     public function it_returns_ok_status_when_if_modified_since_is_in_the_past(): void
     {
         $middleware = new LastModified();
+        $requestTime = $this->timeToIfModifiedSince(time() - 5);
 
         $response = $middleware->handle(
-            $this->createRequest('get', '/', time() - 5),
+            $this->createRequest('get', '/', $requestTime),
             fn() => new Response(),
         );
 
@@ -28,38 +29,42 @@ class LastModifiedTest extends TestCase
     public function it_returns_not_modified_when_if_modified_since_is_in_the_present(): void
     {
         $middleware = new LastModified();
+        $requestTime = $this->timeToIfModifiedSince(time());
 
         $response = $middleware->handle(
-            $this->createRequest('get', '/', time()),
+            $this->createRequest('get', '/', $requestTime),
             fn() => new Response(),
         );
 
         $this->assertSame(304, $response->getStatusCode());
+        $this->assertTrue($response->headers->has('Last-Modified'));
     }
 
     #[Test]
     public function it_returns_not_modified_when_if_modified_since_is_in_the_future(): void
     {
         $middleware = new LastModified();
+        $requestTime = $this->timeToIfModifiedSince(time() + 5);
 
         $response = $middleware->handle(
-            $this->createRequest('get', '/', time()),
+            $this->createRequest('get', '/', $requestTime),
             fn() => new Response(),
         );
 
         $this->assertSame(304, $response->getStatusCode());
-    }
-
-    private function createRequest(string $method, string $uri, int $time): Request
-    {
-        $request = BaseRequest::create($method, $uri);
-        $request->headers->set('If-Modified-Since', $this->timeToIfModifiedSince($time));
-
-        return Request::createFromBase($request);
+        $this->assertTrue($response->headers->has('Last-Modified'));
     }
 
     private function timeToIfModifiedSince(int $time): string
     {
         return date('D, d M Y H:i:s \G\M\T', $time);
+    }
+
+    private function createRequest(string $method, string $uri, string $time): Request
+    {
+        $request = BaseRequest::create($method, $uri);
+        $request->headers->set('If-Modified-Since', $time);
+
+        return Request::createFromBase($request);
     }
 }
