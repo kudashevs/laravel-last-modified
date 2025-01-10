7064c49e83bcff95924df78ca3e56e2633ee1a10
Sergey Kudashev <kudashevs@gmail.com>
2025-01-10 20:26:55 +0300

    Update Providers add a LastModifiedServiceProvider provider

---
+++ src/Providers/LastModifiedServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kudashevs\LaravelLastModified\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class LastModifiedServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+    }
+}
