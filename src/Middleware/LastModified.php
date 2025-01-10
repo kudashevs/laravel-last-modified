ad84f34f157334c23b45edf5ec918d899679aa0a
Sergey Kudashev <kudashevs@gmail.com>
2025-01-10 22:31:00 +0300

    Refactor LastModified make class final

--- src/Middleware/LastModified.php
+++ src/Middleware/LastModified.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class LastModified
+final class LastModified
 {
     /**
      * Handle an incoming request.
